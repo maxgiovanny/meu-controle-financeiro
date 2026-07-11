9e1b2b9393556cad5cb9de3234576cdabfebd48a
maxgiovanny <max.giovanny3@gmail.com>
2026-07-10 23:04:35 -0300

    Atualizar o sidebar.py

--- modulos/sidebar.py
+++ modulos/sidebar.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from modulos.bd_google import salvar_dados_nuvem
+
 from modulos.utilidades import formatar_moeda_br, safe_float, safe_int, safe_str, safe_bool, MESES
 from modulos.bd_google import salvar_dados_nuvem
 
